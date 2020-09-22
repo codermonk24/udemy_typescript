04c98456bad238d985392c218f289d791b8f161b
Mrinal Singh <singhmrinal19@gmail.com>
2020-09-22 12:58:14 +0530

    .ts file added

---
+++ type-anno.ts
@@ -0,0 +1,5 @@
+//type-annotations + type inference
+//variables,functions,objects
+
+//type annotations -- code we add to tell typescript what type of value a variablee will refer to -- developers tell TS the type
+//type inference -- typescript tries to figure out what type of value a variable refers to -- TS guesses the type
